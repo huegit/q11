04e6d75ec148247efaf81537035ec2c187762fbb
huegit <stefan.hueller@gmail.com>
2013-04-10 18:35:38 +0200

    Startskript übertragen

---
+++ q11projekt_start@git.py
@@ -0,0 +1,8 @@
+# 2013-04-10
+# Willkommen beim Q11-Projekt am IKG, Landsberg'
+# Diese erste 'Projektdatei' wollen wir mit Git versionieren
+
+from visual import *
+
+erde = sphere(material=materials.earth)
+print("Erde erstellt")
