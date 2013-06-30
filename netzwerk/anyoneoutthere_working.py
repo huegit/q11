2a94639a1f97ef9216a7625942f3a031e3305d03
Alexander Epple <epple.alexander@web.de>
2013-06-30 13:47:14 +0200

    Update: + Client als eigene Klasse, wichtig für Senden/Empfangen Ingame + ein zwei kleine Bugfixes

--- netzwerk/anyoneoutthere_working.py
+++ netzwerk/anyoneoutthere_working.py
@@ -5,12 +5,16 @@
 
 from socket import *        # Socket Server importieren
 import threading            # Threading importieren, mögl. später wichtig
+import _thread
 
 MYPORT = 50000              # Port def
 client_new= None            # client_new def
 
 class Server(threading.Thread):
     "Klasse Server, kann neue Clients annehmen"
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
     def run(self):
         "Start() Methode"
         addr = ("", MYPORT)                     # Addresse wird bestimmt, keine IP und MYPORT
@@ -27,23 +31,44 @@ class Server(threading.Thread):
             
             if daten.decode("UTF-8").split()[0] == "conreq":    # Wenn erste Daten conreq am Anfang enthalten
                 self.firststrike()                              # Methode firststrike() wird ausgeführt 
-
+                
     def firststrike(self):
         "Wird nur bei neuem Client ausgeführt"
         server = socket(AF_INET, SOCK_DGRAM)    # Server wird geöffnet
         antwort = "12hnisa".encode("UTF-8")     # def Antwort
         server.sendto(antwort, (client_new))    # Antwort wird an letzte Empfangsaddresse geschickt
+        
+class Client(threading.Thread):
+    "Klasse Client, kann an Server senden und empfangen"
+    def __init__(self, ip, name):
+        threading.Thread.__init__(self)
+        self.ip   = ip
+        self.name = name
+        
+    def run(self):
+        while True:
+            self.schreiben()
+            
+    def schreiben(self):
+        server = socket(AF_INET, SOCK_DGRAM)
+        senden = input("Nachricht:").encode("UTF-8")
+        server.sendto(senden, (self.ip, MYPORT))
 
 def serverstart():
     "Startet neuen Server der klasse Server"
     server = Server()
     server.start()
 
+def client(ip, name):
+    "Startet Client der an Server gebunden ist"
+    client = Client(ip, name)
+    client.start()
+
 def serversuche():
     "Sucht Server bzw erstellt neuen"
     s = socket(AF_INET, SOCK_DGRAM)                 # Server wird geöffnet
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, True)    # Option Broadcast wird ausgewählt
-    s.settimeout(3)                                 # Timeout wird festgelegt
+    s.settimeout(1)                                 # Timeout wird festgelegt
 
     print("Versuche Server zu finden...")
 
@@ -54,14 +79,11 @@ def serversuche():
     try:
         antwort, address = s.recvfrom(1024)     # sollte etwas zurückkommen (binnen 3 sec.)
         ip = address[0]                         # wird die IP gespeichert
-        print("Server gefunden:", address[0])   # ausgegeben
         s.close                                 # der Suchserver wird geschlossen
-
-        while True:
-            server = socket(AF_INET, SOCK_DGRAM)     # ein neuer Server geöffnet
-            raw = input("Nachricht:").encode("UTF-8")# man kann eine Nachricht schicken
-            server.sendto(raw, (ip, MYPORT))         # wird an gespeicherte IP gesendet
-
+        if antwort.decode("UTF-8") == "12hnisa":
+            print("Server gefunden:", ip)
+            client(ip, myname)
+            
     except timeout:                 # wenn nach 3 sec kein Server gefunden wurde
         print("Kein Server gefunden, erstelle neuen auf Port", MYPORT)
         serverstart()               # wird ein neuer erstellt
